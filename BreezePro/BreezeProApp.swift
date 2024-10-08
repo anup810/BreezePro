55c5630eedc14565a8788c960314950c574be502
Anup Saud <anupsaud10@gmail.com>
2024-10-08 18:29:43 -0400

    Initial Commit

---
+++ BreezePro/BreezeProApp.swift
@@ -0,0 +1,17 @@
+//
+//  BreezeProApp.swift
+//  BreezePro
+//
+//  Created by Anup Saud on 2024-10-08.
+//
+
+import SwiftUI
+
+@main
+struct BreezeProApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
